de587b7d95a6d76968181153ad70e0ec58bed3c9
João Vieira <mutantinotrue@outlook.com>
2025-08-19 18:09:09 -0300

    Criando SairException

---
+++ src/br/com/campominado/excecao/SairException.java
@@ -0,0 +1,8 @@
+package br.com.campominado.excecao;
+
+import java.io.Serial;
+
+public class SairException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+}
