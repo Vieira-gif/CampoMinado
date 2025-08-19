95e779ac66b1a7f9f612ffcd2f8a34ac393a8915
João Vieira <mutantinotrue@outlook.com>
2025-08-19 12:32:45 -0300

    parametrizando a classe tabuleiro

--- src/br/com/campominado/modelo/Tabuleiro.java
+++ src/br/com/campominado/modelo/Tabuleiro.java
@@ -1,4 +1,41 @@
 package br.com.campominado.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
+    private int linhas;
+    private int colunas;
+    private int minas;
+
+    private final List<Campo> campos  = new ArrayList<Campo>();
+
+    public Tabuleiro(int linhas, int colunas, int minas) {
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.minas = minas;
+
+        gerarCampos();
+        associarOsVizinhos();
+        sortearMinas();
+    }
+
+    private void sortearMinas() {
+        for (int linhas = 0; linhas < this.linhas; linhas++) {
+            for (int coluna = 0; coluna < colunas; coluna++) {
+                campos.add(new Campo(linhas, coluna));
+            }
+        }
+    }
+
+    private void associarOsVizinhos() {
+        for(Campo c1 : campos) {
+            for(Campo c2 : campos) {
+                c1.adicionarVizinho(c2);
+            }
+        }
+    }
+
+    private void gerarCampos() {
+    }
 }
